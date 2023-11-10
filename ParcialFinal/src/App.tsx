5ffd4637048f5413de5ee9d319df292c58963fe1
Refered <zap728@amerike.edu.mx>
2023-11-10 12:42:01 -0600

    Instalacion y tailwind

---
+++ ParcialFinal/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-indigo-500 w-screen h-screen " >
+        <div className="flex flex-col grow p-5 items-center text-2xl ">  Control de gastos
+        <form className="border bg-indigo-100 ">
+          s
+        </form>
+        </div>
+        </div>
+ 
+    </>
+  )
+}
+
+export default App
